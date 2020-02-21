6bb1d03bb1c75b4786bb8eb600146278f8e4d19f
Igor Panigada <ipanigada@gmail.com>
2020-02-21 18:25:41 +0100

    added comments to code (to test git commit)

--- create_backup_table.sql
+++ create_backup_table.sql
@@ -1,9 +1,11 @@
+-- create table as is a function to fully duplicate a table
 create table sakila.public.backup2
 as
 select *
 from customer c 
 ;
 
+-- code below visualize the table created before
 select *
 from backup2
 ;
