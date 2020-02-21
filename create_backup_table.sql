022e93b8856d35b01d337619c86363f3a205bcde
Igor Panigada <ipanigada@gmail.com>
2020-02-21 19:35:14 +0100

    added columns and imported new data

--- create_backup_table.sql
+++ create_backup_table.sql
@@ -9,3 +9,25 @@ from customer c
 select *
 from backup2
 ;
+
+select max(length(first_name))
+from customer;
+
+select max(length(last_name))
+from customer;
+
+alter table customer 
+	add column 
+	username varchar (8)
+;
+alter table customer 
+add column full_name varchar (25)
+;
+
+select * from customer c2;
+
+update customer
+ set username = lower(concat(substring(first_name,1,1), substring(last_name,1,7)));
+ 
+update customer 
+	set full_name = INITCAP(CONCAT (first_name, ' ', last_name));
